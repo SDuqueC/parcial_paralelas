5207e7069c134d6cc98de654986bb6fb9d310302
Juan CV <jacklazerjscv@gmail.com>
2024-05-03 21:44:47 -0500

    Avance 3 en one_thread.py y modificacion de otros archivos

--- descarga_video.py
+++ descarga_video.py
@@ -22,10 +22,18 @@ def download_video(url, nombre_archivo_salida, numero_de_video):
         # Ejecutar el comando yt-dlp para descargar el video
         proceso = subprocess.run(["yt-dlp", "--playlist-item", numero_de_video, "-o", nombre_archivo_salida, url], capture_output=True, text=True)
 
+        # Esta madre es para encontrar la fecha de subida del video a youtube
+        fecha_de_subida_en_consola = subprocess.run(['yt-dlp', '--print', 'upload_date', '--playlist-item', numero_de_video, url], capture_output=True, text=True)
+        fecha_de_subida = fecha_de_subida_en_consola.stdout
+
+        # Y esta otra es para encontrar la url del video
+        id_en_consola = subprocess.run(['yt-dlp', '--print', 'id', '--playlist-item', numero_de_video, url], capture_output=True, text=True)
+        url_real = "https://www.youtube.com/watch?v=" + id_en_consola.stdout
+
         nombre_archivo_salida_con_formato = nombre_archivo_salida + ".webm"
         if proceso.returncode == 0:
             print(f"El video {nombre_archivo_salida_con_formato} ha sido descargado correctamente.")
-            return True
+            return fecha_de_subida, url_real
         else:
             print(f"Error al descargar el video {nombre_archivo_salida_con_formato}: {proceso.stderr}")
             return False
--- one_thread.py
+++ one_thread.py
@@ -1,27 +1,74 @@
 import os
+import json
+import datetime
 import lector_json
 import descarga_video
 import extraer_audio
 
-# nombre_v_a = "Faded Into You"
+
+
+
+
+datos_de_descargas_inicial = {
+    # "descarga X": {
+    #     "video": "video X",
+    #     "url": "url X",
+    #     "fecha de subida a youtube": "fecha de subida X",
+    #     "fecha de descarga": "fecha de descarga X",
+    # }
+}
+
+nombre_archivo = "registro_de_descargas.json"
+
+with open(nombre_archivo, "w") as archivo_json:
+    json.dump(datos_de_descargas_inicial, archivo_json)
+
+
+
+
 
 urls_y_nombres = lector_json.leer_json('urls_y_nombres.json')
 
+
+
+
+
 for url_y_nombre in urls_y_nombres:
 
-    for numero_de_video in range(1, 6):
+    for numero_de_video in range(1, 2):
 
         numero_de_video_str = str(numero_de_video)
         video = url_y_nombre[1] + numero_de_video_str
 
-        descarga_video.download_video(url_y_nombre[0], video, numero_de_video_str)
+        fecha_de_subida, url_real = descarga_video.download_video(url_y_nombre[0], video, numero_de_video_str)
 
         video = video + '.webm'
         audio = url_y_nombre[1] + numero_de_video_str + '.mp3'
 
         extraer_audio.extract_audio(video, audio)
 
-        archivo = video
 
-        os.remove(archivo)
+
+        os.remove(video)
         print(f"El video {video} ha sido eliminado correctamente.")
+
+
+
+        with open("registro_de_descargas.json", "r") as archivo_json:
+            datos_de_descargas = json.load(archivo_json)
+
+        numero_descargas = len(datos_de_descargas)
+
+        descarga_X = {
+            "video": video,
+            "audio": audio,
+            "url": url_real,
+            "fecha de subida a youtube": fecha_de_subida,
+            "fecha de descarga": datetime.datetime.now().strftime("%Y-%m-%d %H")
+        }
+
+        descarga_x = "descarga " + str(numero_descargas + 1)
+        datos_de_descargas[descarga_x] = descarga_X
+
+        with open("registro_de_descargas.json", "w") as archivo_json:
+            json.dump(datos_de_descargas, archivo_json, indent=4) 
